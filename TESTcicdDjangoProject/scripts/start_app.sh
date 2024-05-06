0b881de3059ce307911dfaccec9b90b31be1ddc9
Muahmmadadilkhan-46 <adilkhanm046@gmail.com>
2024-05-06 09:39:17 +0500

    cahnging server ip

--- TESTcicdDjangoProject/scripts/start_app.sh
+++ TESTcicdDjangoProject/scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["18.141.175.250"]/' /home/ubuntu/TESTcicdDjangoProject/TESTcicdDjangoProject/settings.py
+sed -i 's/\[]/\["13.229.114.6"]/' /home/ubuntu/TESTcicdDjangoProject/TESTcicdDjangoProject/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
